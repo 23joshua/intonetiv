3c003073bd1764a5601e4a438f8a0a9967778bae
joshua <34164669+23joshua@users.noreply.github.com>
2022-03-07 23:39:45 +0200

    atualizandos os links das paginas

--- index.php
+++ index.php
@@ -13,14 +13,33 @@
 		<div class="container">
 			<div class="logo"><a href="">logo</a></div>
 			<nav class="nav">
-				<a href="">home</a>
-				<a href="">about</a>
-				<a href="">blog</a>
+				<a href="./">home</a>
+				<a href="?file=about">about</a>
+				<a href="?file=blog">blog</a>
 			</nav>
 			<div class="head-button">
-				<a href="" class="btn contact">contacto</a>
+				<a href="" class="btn btn_blue contact">contacto</a>
 			</div>
 		</div>
 	</header>
+
+	<main>
+		<?php  
+
+			$file = filter_input(INPUT_GET, "file", FILTER_SANITIZE_SPECIAL_CHARS);
+			if (empty("$file")) {
+				require __DIR__ . "/views/home.php";
+			}elseif($file && file_exists(__DIR__ . "/views/{$file}.php")){
+				require __DIR__ . "/views/{$file}.php";
+			}else{
+				require __DIR__ . "/views/404.php";
+			}
+
+		?>
+	</main>
+
+	<footer class="footer">
+		
+	</footer>
 </body>
 </html>
---
+++ views/blog.php
@@ -0,0 +1,3 @@
+<div class="container">
+	<h1>blog</h1>
+</div>
