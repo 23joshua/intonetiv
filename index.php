986bc2187dc9ffb4e60f934aeb6329a0cb9a84e4
joshua <34164669+23joshua@users.noreply.github.com>
2022-03-07 23:13:23 +0200

    criacao dos header
    criação do menu geral do site

--- index.php
+++ index.php
@@ -4,9 +4,23 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>InTunetive SaaS</title>
-	<link rel="stylesheet" href="">
+	<link rel="stylesheet" href="assets/css/reset.css">
+	<link rel="stylesheet" href="assets/css/boot.css">
+	<link rel="stylesheet" href="assets/css/styles.css">
 </head>
 <body>
-	
+	<header class="header">
+		<div class="container">
+			<div class="logo"><a href="">logo</a></div>
+			<nav class="nav">
+				<a href="">home</a>
+				<a href="">about</a>
+				<a href="">blog</a>
+			</nav>
+			<div class="head-button">
+				<a href="" class="btn contact">contacto</a>
+			</div>
+		</div>
+	</header>
 </body>
 </html>
