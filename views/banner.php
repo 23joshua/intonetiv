c409b5d1895cc3d8c9a18ff5f7c6ec83534612e0
joshua <34164669+23joshua@users.noreply.github.com>
2022-03-12 13:15:43 +0200

    actulizando a respossividade do site

--- views/banner.php
+++ views/banner.php
@@ -3,7 +3,7 @@
 		<div class="banner-content">
 			<h1>Do you want to manage social? Get started with Intunetive!</h1>
 			<p>Sign up, launch your accounts and count on powerful automations to manage everything while you have a good coffee</p>
-			<a href="" class="btn btn_blue btn_big btn-banner transition"><ion-icon name="log-in-outline"></ion-icon> create an account</a>
+			<a href="" class="btn btn_blue btn_big btn-banner transition"><ion-icon name="log-in-outline"></ion-icon> create account</a>
 		</div>
 	</div>
 </article>
